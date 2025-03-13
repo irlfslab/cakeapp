7c16c17ca6c66cfb3e097ba740ce6d457423e9a1
IL <53288945+irlfslab@users.noreply.github.com>
2025-03-12 20:12:21 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+tailwind.config ={
+    darkMode: 'class',
+    theme: {
+        extend: {
+            colors: {
+                "logo": {
+                    700: "#1c80ae",
+                    800: "#11469a",
+                }
+            }
+        }
+    }
+}
